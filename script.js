4aa475d51fc55a4be97966b24c2a878b9d019e9a
Matias Blanco <100545487+MatiasDavidBlanco@users.noreply.github.com>
2022-08-02 11:30:10 -0300

    a

---
+++ script.js
@@ -0,0 +1,14 @@
+/*CARRUSEL PRODUCTOS*/
+
+window.addEventListener("load", function(){
+    new Glider(document.querySelector(".carousel__lista"),{
+        slidesToShow: 4,
+        slidesToScroll: 5,
+        draggable: true,
+        dots: ".carousel__indicadores",
+        arrow: {
+            prev: ".carousel__anterior",
+            next: ".carousel__siguiente"
+        }
+    });
+});
