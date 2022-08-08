f68b9e12d71a40467cc1cb70d62cdde8c479ac7b
Andriy Palianytsia <andriy.palianytsia@gmail.com>
2022-08-08 16:40:17 -0300

    Andriy

--- ModuleScript.js
+++ ModuleScript.js
@@ -13,26 +13,27 @@ class moduleMenu extends HTMLElement {
     render() {
         this.shadow.innerHTML = `
         <!--html-->
+  <link rel="stylesheet" href="css/productosProductos.css">
           <div class="carousel__lista">
             <div class="carousel__elemento">
-              <a href=""><img src="img/img productos/cocinas.png" alt="cocinas"></a>
-              <p>COCINAS</p>
+              <a href=""><img src="img/img productos/cocinas.png" alt="cocinas">
+              <p>COCINAS</p></a>
             </div>
             <div class="carousel__elemento">
-              <a href=""><img src="img/img productos/racks.png" alt="racks"></a>
-              <p>RACKS</p>
+              <a href=""><img src="img/img productos/racks.png" alt="racks">
+              <p>RACKS</p></a>
             </div>
             <div class="carousel__elemento">
-              <a href=""><img src="img/img productos/placards.png" alt="placards"></a>
-              <p>PLACARDS</p>
+              <a href=""><img src="img/img productos/placards.png" alt="placards">
+              <p>PLACARDS</p></a>
             </div>
             <div class="carousel__elemento">
-              <a href=""><img src="img/img productos/cocinas.png" alt="cocinas"></a>
-              <p>CAMAS</p>
+              <a href=""><img src="img/img productos/cocinas.png" alt="cocinas">
+              <p>CAMAS</p></a>
             </div>
             <div class="carousel__elemento">
-              <a href=""><img src="img/img productos/racks.png" alt="placards"></a>
-              <p>CAMAS</p>
+              <a href=""><img src="img/img productos/racks.png" alt="placards">
+              <p>CAMAS</p></a>
             </div>
         <!--!html-->
         `;
