962c103168bebf581e2a6f8fde42702a8f6af867
Andriy Palianytsia <53368889+AndriyPalianytsia@users.noreply.github.com>
2022-08-22 17:27:40 -0300

    andriy

--- ModuleScript.js
+++ ModuleScript.js
@@ -4,7 +4,7 @@ var vwimg = document.querySelector('.imgConteiner');
 function buildIMG(carpeta){
   let imgs = "";
   for(let i=1;i<40;i++){
-    imgs += `<img src="img/${carpeta}/${carpeta} (${i}).jpg" alt="${carpeta}" onClick="changeIMG(${carpeta},${i})" onerror="this.style.display='none'">`
+    imgs += `<img src="img/${carpeta}/${carpeta} (${i}).jpg" alt="${carpeta}" onClick="changeIMG('${carpeta}',${i})" onerror="this.style.display='none'">`
   }
   li.innerHTML=imgs;
 }
