6f9ab59f5424c7419440993a9215130ce97747ac
Andriy Palianytsia <53368889+AndriyPalianytsia@users.noreply.github.com>
2022-08-22 15:53:41 -0300

    js update

--- ModuleScript.js
+++ ModuleScript.js
@@ -4,10 +4,10 @@ var vwimg = document.querySelector('.imgConteiner');
 function buildIMG(carpeta){
   let imgs = "";
   for(let i;i<10;i++){
-    imgs += `<img src="img/${carpeta}/img${i}.png" alt="${carpeta}" onClick="changeIMG(i)">`
+    imgs += `<img src="img/${carpeta}/${carpeta}(${i}).jpg" alt="${carpeta}" onClick="changeIMG(i)">`
   }
   li.innerHTML=imgs;
 }
 function changeIMG(i){
-  vwimg.innerHTM=`<img src="img/${carpeta}/img${i}.png" alt="${carpeta}">`
+  vwimg.innerHTM=`<img src="img/${carpeta}/${carpeta}(${i}).jpg" alt="${carpeta}">`
 }
