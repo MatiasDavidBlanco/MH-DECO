dbde2e38cbd3505d5c82a9ec34a7b545d20089b8
Andriy Palianytsia <53368889+AndriyPalianytsia@users.noreply.github.com>
2022-08-23 10:38:29 -0300

    update js Modulescript change name camas

--- ModuleScript.js
+++ ModuleScript.js
@@ -1,13 +1,22 @@
 var li = document.querySelector('.imgsConteiner');
 var vwimg = document.querySelector('.imgConteiner');
+var a = ""
 
-function buildIMG(carpeta){
+function changeVar(value) {
+  window.location.href = '../MH-DECO/product.html'; 
+  a = value;
+}
+function loadPage() {
+  buildIMG(a);
+  changeIMG(a,1);
+}
+function buildIMG(carpeta) {
   let imgs = "";
-  for(let i=1;i<40;i++){
+  for (let i = 1; i < 40; i++) {
     imgs += `<img src="img/${carpeta}/${carpeta} (${i}).jpg" alt="${carpeta}" onClick="changeIMG('${carpeta}',${i})" onerror="this.style.display='none'">`
   }
-  li.innerHTML=imgs;
+  li.innerHTML = imgs;
 }
-function changeIMG(carpeta,i){
-  vwimg.innerHTML=`<img src="img/${carpeta}/${carpeta} (${i}).jpg" alt="${carpeta}">`
+function changeIMG(carpeta, i) {
+  vwimg.innerHTML = `<img src="img/${carpeta}/${carpeta} (${i}).jpg" alt="${carpeta}">`
 }
